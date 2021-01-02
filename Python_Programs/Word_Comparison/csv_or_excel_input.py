25bd04e24b3e61dae0fddef916a2956ca9a1e36e
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2021-01-03 00:05:08 +0530

    removed comments

--- Python_Programs/Word_Comparison/csv_or_excel_input.py
+++ Python_Programs/Word_Comparison/csv_or_excel_input.py
@@ -8,13 +8,6 @@ try:
 except UnicodeDecodeError:
     df = pd.read_excel(csv_file).reset_index(drop = True)
     csv = False
-'''
-df['TfIdf_Custom_n-gram'] = df.apply(lambda x: word_compare_test.compare_words1(x['String1'], x['String2']), axis = 1)
-df['TfIdf_word_analyzer'] = df.apply(lambda x: word_compare_test.compare_words2(x['String1'], x['String2']), axis = 1)
-df['TfIdf_char_analyzer'] = df.apply(lambda x: word_compare_test.compare_words3(x['String1'], x['String2']), axis = 1)
-df['My_char_Vectorizer'] = df.apply(lambda x: word_compare_test.compare_words4(x['String1'], x['String2']), axis = 1)
-df['My_word_Vectorizer'] = df.apply(lambda x: word_compare_test.compare_words5(x['String1'], x['String2']), axis = 1)
-'''
 
 for row in range(df.shape[0]):
     if pd.isna(df.loc[row, 'TfIdf_Custom_n-gram']):
