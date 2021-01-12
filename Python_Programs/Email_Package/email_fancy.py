59f8722932dea65adb0fff560f9e8676c42883a0
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2021-01-12 21:50:36 +0530

    started fancy email

--- Python_Programs/Email_Package/email_fancy.py
+++ Python_Programs/Email_Package/email_fancy.py
@@ -1,27 +1,6 @@
-import os, re, sys
+import smtplib, ssl, os, re, sys
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-
-def bold(text):
-    return '<b>' + text + '</b>'
-
-def italic(text):
-    return '<i>' + text + '</i>'
-
-def underline(text):
-    return '<u>' + text + '</u>'
-
-def break():
-    return '<br>'
-
-def newLine():
-    return '<br>'
-
-def link(text = None, url = None):
-    if ((text is None) and (url is None)):
-        return None
-    elif ((text is None) and (url is not None)):
-        text = ""
-    elif ((text is not None) and (url is None)):
-        url = 'https://www.google.com/search?q=' + text
-
-    return "<a href=\"" + url + "\">" + text + "</a>"
