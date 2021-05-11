7fdfa1c6729484f555ac1b548f7ebbf851d7125f
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2021-05-11 12:50:56 +0530

    Finding total number of lines

--- Cpp_Programs/Static_Code_Analysis/CppStaticCodeAnalysis/CppStaticCodeAnalysis/CppStaticCodeAnalysis.cpp
+++ Cpp_Programs/Static_Code_Analysis/CppStaticCodeAnalysis/CppStaticCodeAnalysis/CppStaticCodeAnalysis.cpp
@@ -1,20 +1,31 @@
-// CppStaticCodeAnalysis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string code_filename, line;
+    char temp = 32;
+    long line_count = 0;
+    ifstream fin;
+
+    cout << "Please specify the code file: ";
+    cin >> code_filename;
+
+    fin.open(code_filename, ios::in);
+
+    while (!fin.eof())
+    {
+        temp = fin.get();
+        if (temp == 10)
+        {
+            line_count++;
+        } 
+    }
+
+    cout << "Total number of lines: " << line_count;
+
+    fin.close();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
