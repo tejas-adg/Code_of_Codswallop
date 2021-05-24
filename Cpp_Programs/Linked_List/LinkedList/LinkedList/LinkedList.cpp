5f4369dd07c9fb0385166a4e006729827d59e516
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2021-05-25 01:03:03 +0530

    Added doubly linked list, add works

--- Cpp_Programs/Linked_List/LinkedList/LinkedList/LinkedList.cpp
+++ Cpp_Programs/Linked_List/LinkedList/LinkedList/LinkedList.cpp
@@ -1,64 +1,92 @@
 #include "Single_Linked_List.h"
+#include "Doubly_Linked_List.h"
 
 using namespace std;
 
-char main()
+int main()
 {
-	Single_Linked_List<char> s_ll;
+	Doubly_Linked_List<char> d_ll;
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('t'));
+	d_ll.addItem(Double_Link_Node<char>::create_node('t'));
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('e'), 1);
+	d_ll.addItem(Double_Link_Node<char>::create_node('e'), 1);
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('j'), 2);
+	d_ll.addItem(Double_Link_Node<char>::create_node('j'), -1);
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('a'), 3);
+	d_ll.addItem(Double_Link_Node<char>::create_node('a'), -1);
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('s'), 4);
+	d_ll.addItem(Double_Link_Node<char>::create_node('s'), -1);
 
-	s_ll.addItem(Single_Link_Node<char>::createNode('Z'), 3);
-	
-	s_ll.Print_List(); cout << endl;
+	d_ll.addItem(Double_Link_Node<char>::create_node('M'), 5);
 
-	delete s_ll.removeItemByPosition(1);
+	d_ll.Print_List(); cout << endl;
 
-	s_ll.Print_List(); cout << endl;
+	d_ll.addItem(Double_Link_Node<char>::create_node('A'), 5);
 
-	s_ll.Reverse_List();
+	d_ll.Print_List(); cout << endl;
 
-	s_ll.Print_List(); cout << endl;
-
-	s_ll.Reverse_List();
-
-	s_ll.Print_List(); cout << endl;
-
-	delete s_ll.removeItemByData('a');
-	
-	s_ll.Print_List(); cout << endl;
-
-	delete s_ll.removeItemByData('t');
-
-	s_ll.Print_List(); cout << endl;
-
-	delete s_ll.removeItemByPosition(0);
-	delete s_ll.removeItemByPosition(-1);
-	delete s_ll.removeItemByPosition(0);
-
-	s_ll.Print_List(); cout << endl;
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('t'));
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('e'), 1);
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('j'), 2);
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('a'), 3);
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('s'), 4);
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('a'), -1);
-
-	s_ll.addItem(Single_Link_Node<char>::createNode('m'), -1);
-
-	s_ll.Print_List(); cout << endl;
+	return 0;
 }
+
+//char main()
+//{
+//	Single_Linked_List<char> s_ll;
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('t'));
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('e'), 1);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('j'), 2);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('a'), 3);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('s'), 4);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('Z'), 3);
+//	
+//	s_ll.Print_List(); cout << endl;
+//
+//	delete s_ll.removeItemByPosition(1);
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	s_ll.Reverse_List();
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	s_ll.Reverse_List();
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	delete s_ll.removeItemByData('a');
+//	
+//	s_ll.Print_List(); cout << endl;
+//
+//	delete s_ll.removeItemByData('t');
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	delete s_ll.removeItemByPosition(0);
+//	delete s_ll.removeItemByPosition(-1);
+//	delete s_ll.removeItemByPosition(0);
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('t'));
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('e'), 1);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('j'), 2);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('a'), 3);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('s'), 4);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('a'), -1);
+//
+//	s_ll.addItem(Single_Link_Node<char>::createNode('m'), -1);
+//
+//	s_ll.Print_List(); cout << endl;
+//
+//	return 0;
+//}
--- Cpp_Programs/Linked_List/LinkedList/LinkedList/Nodes.h
+++ Cpp_Programs/Linked_List/LinkedList/LinkedList/Nodes.h
@@ -68,14 +68,14 @@ void Single_Link_Node<T>::setNextNode(Single_Link_Node<T>* _next_Node)
 	nextNode = _next_Node;
 }
 
-/*
+
 template<typename T>
-typename Double_Link_Node
+class Double_Link_Node
 {
 private:
 	T obj;
 	Double_Link_Node<T>* nextNode;
-	Double_Link_Node<T>* PreviousNode;
+	Double_Link_Node<T>* previousNode;
 
 public:
 	Double_Link_Node();
@@ -85,6 +85,71 @@ public:
 	void setData(T);
 	Double_Link_Node<T>* getNextNode(void);
 	void setNextNode(Double_Link_Node<T>*);
-};*/
+	Double_Link_Node<T>* getPreviousNode(void);
+	void setPreviousNode(Double_Link_Node<T>*);
+	static Double_Link_Node<T>* create_node(T);
+};
 
+template<typename T>
+Double_Link_Node<T>* Double_Link_Node<T>::create_node(T obj)
+{
+	Double_Link_Node<T>* new_node_ptr = new Double_Link_Node<T>();
+	new_node_ptr->setData(obj);
+	new_node_ptr->setNextNode(nullptr);
+	new_node_ptr->setPreviousNode(nullptr);
+
+	return new_node_ptr;
+}
+
+template<typename T>
+Double_Link_Node<T>::Double_Link_Node()
+{
+	obj = 0;
+	nextNode = nullptr;
+	previousNode = nullptr;
+}
+
+template<typename T>
+Double_Link_Node<T>::~Double_Link_Node()
+{
+	obj = 0;
+	nextNode = nullptr;
+	previousNode = nullptr;
+}
+
+template<typename T>
+T Double_Link_Node<T>::getData()
+{
+	return obj;
+}
+
+template<typename T>
+void Double_Link_Node<T>::setData(T _obj)
+{
+	obj = _obj;
+}
+
+template<typename T>
+Double_Link_Node<T>* Double_Link_Node<T>::getNextNode()
+{
+	return nextNode;
+}
+
+template<typename T>
+void Double_Link_Node<T>::setNextNode(Double_Link_Node<T>* _nextNode)
+{
+	nextNode = _nextNode;
+}
+
+template<typename T>
+Double_Link_Node<T>* Double_Link_Node<T>::getPreviousNode()
+{
+	return previousNode;
+}
+
+template<typename T>
+void Double_Link_Node<T>::setPreviousNode(Double_Link_Node<T>* _previousNode)
+{
+	previousNode = _previousNode;
+}
 #endif
