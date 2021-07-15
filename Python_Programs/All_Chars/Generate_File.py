15497d05a4ba8c5e801b3c84d346e73e02ef7ee6
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2021-07-16 00:20:33 +0530

    used python string formatters

--- Python_Programs/All_Chars/Generate_File.py
+++ Python_Programs/All_Chars/Generate_File.py
@@ -1,9 +1,8 @@
 import os
 
-f = open(os.path.join(os.path.abspath(os.path.join(__file__, '..')), 'num_and_char.txt'), 'w', encoding = 'utf-32')
-
-for num in range(0x110000):
-    f.write(rf'{num}: {chr(num)}')
-    f.write('\n')
+f = open(os.path.join(os.path.abspath(os.path.join(__file__, '..')), 'num_and_char.txt'), 'w', encoding = 'utf_8')
+for num in range(0x100):
+	f.write(f'{num:03d}:0x{num:02x}:{num:08b}:{num:1c}')
+	f.write('\n')
 
 f.close()
